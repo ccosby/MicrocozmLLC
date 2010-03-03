23acf086944c6ae03f7bf0db8788d7a4e64352c4
Chris Cosby <chris@microcozm.net>
2010-03-03 12:57:40 -0500

    Initial commit of conversion from Ogg::Vorbis::Header::PurePerl-1.0

---
+++ src/net/microcozm/OggTag/OggVorbisHeader.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.microcozm.OggTag;
+
+/**
+ *
+ * @author cosbyc
+ */
+public class OggVorbisHeader {
+
+	// First four bytes of stream are always OggS
+	private static final String OGGHEADERFLAG = "OggS";
+
+	public OggVorbisHeader() {
+	}
+
+	public void info() {
+	}
+
+	public void comment_tags() {
+	}
+
+	public void comment() {
+	}
+
+	public void path() {
+	}
+
+	public void _init() {
+	}
+
+	public void _skipID3Header() {
+	}
+
+	public void _checkHeader() {
+	}
+
+	public void _loadInfo() {
+	}
+
+	public void _loadComments() {
+	}
+
+	public void _processComments() {
+	}
+
+	public void Get8u() {
+	}
+
+	public void Get32u() {
+	}
+
+	public void _calculateTrackLength() {
+	}
+
+	public void _decodeInt() {
+	}
+}
