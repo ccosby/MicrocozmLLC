3b74eaaa4c723cd589e0c439c9e51c90b7ee914b
Chris Cosby <ccosby@gmail.com>
2013-05-15 15:01:19 -0400

    Make Perl templates resist Perl::Critic's brutal template

--- templates/TemplatePackage.pm
+++ templates/TemplatePackage.pm
@@ -1,51 +1,63 @@
-#!/usr/bin/env perl -w
+#!/usr/bin/env perl
 
 package TemplatePackage;
+use base qw(Exporter);
 
 use strict;
+use warnings;
 
-use Carp ();
-use Exporter;
+use Carp         ();
 use Scalar::Util ();
 
 # The basics of an Exporter package
-my $VERSION   = '1.0-SNAPSHOT';
-my @ISA       = qw(Exporter);
-my @EXPORT    = ();
-my @EXPORT_OK = ();
+our $VERSION     = '1.0-SNAPSHOT';
+our @EXPORT_OK   = ();
+our %EXPORT_TAGS = ( all => [qw(&smart_sub &instance_sub)] );
 
-__PACKAGE__->main() unless caller;
-
-my $isa = sub { Scalar::Util::blessed $_[0] and $_[0]->isa($_[1]); };
-
-sub smart_sub
-{
-	# This allows us to call smart_sub as an instance or a self method
-	my $self = shift;
-	unshift(@_, $self) unless $self->$isa(__PACKAGE__);
-	1;
+if ( !caller ) {
+    __PACKAGE__->main();
 }
 
-sub instance_sub
-{
-	my $self = shift;
-	Carp::croak 'instance_sub is only allowed in instance context' unless $self->$isa(__PACKAGE__);
+my $isa = sub { Scalar::Util::blessed $_[0] and $_[0]->isa( $_[1] ); };
+
+sub smart_sub {
+
+    # This allows us to call smart_sub as an instance or a self method
+    # Note that this doesn't work properly if called from a caller() method.
+    my ( $self, @args ) = @_;
+    if ( !$self->$isa(__PACKAGE__) ) {
+        unshift @args, $self;
+    }
+    use Data::Dumper ();
+    print Data::Dumper::Dumper( \@args ) or Carp::carp 'print failed';
+    return;
 }
 
-sub new
-{
-	my $class = shift;
-	my (undef, undef) = my @original_args = @_;
-
-	my $self = ();
-	$self->{name} = undef;
-	return ( bless( $self, $class ) );
+sub instance_sub {
+    my ( $self, $arg_ref ) = @_;
+    if ( !$self->$isa(__PACKAGE__) ) {
+        Carp::croak 'instance_sub is only allowed in instance context';
+    }
+    return;
 }
 
-sub main
-{
-	my $self = shift;
-	printf "This is %s::main()\n", __PACKAGE__;
+sub new {
+    my ( $class, $arg_ref ) = my @original_args = @_;
+
+    my $self = ();
+    $self->{name} = undef;
+    return ( bless $self, $class );
+}
+
+sub main {
+    my $self = shift;
+
+    $self->smart_sub(qw(a b c d e));
+
+    #smart_sub(qw(a b c d e));
+
+    printf "This is %s::main()\n", __PACKAGE__;
+    return;
 }
 
 1;
--- templates/template.pl
+++ templates/template.pl
@@ -1,9 +1,13 @@
-#!/usr/bin/env perl -w
+#!/usr/bin/env perl
 
 use strict;
+use warnings;
+
+use version; our $VERSION = qv('1.0.0');
+
 use TemplatePackage;
 
-my $t_pkg = new TemplatePackage();
+my $t_pkg = TemplatePackage->new();
 $t_pkg->main();
 
 # vim: set ts=4 sw=4 noexpandtab fdm=marker filetype=perl :
